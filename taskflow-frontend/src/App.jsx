4d5e1b81a1e45aae2fded9367cf8353130e59e1c
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2025-04-19 22:26:39 +0100

    refactor: moved protected route logic to a separate function

--- taskflow-frontend/src/App.jsx
+++ taskflow-frontend/src/App.jsx
@@ -3,33 +3,27 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Landing, Register, Login, Dashboard, NotFound, PrivateRoute } from './components/pages';
 import { useAuthContext } from './contexts/AuthContext';
 
-// import PrivateRoute from './components/PrivateRoute';
+const ProtectedRoute = ({children}) => {
+  const { user } = useAuthContext();
+  console.log(user);
+
+  return user ? children : <Navigate to="/login" />;
+};
 
 function App() {
-  const { user } = useAuthContext();
-
-  const ProtectedRoute = ({children}) => {
-    if (!user) {
-      return <Navigate to="/login" />
-    }
-    return children;
-  };
-
   return (
-    <div class="">
+    <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login"  element={<Login />} />
-          <Route path="/dashboard"  element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />          
           <Route path="*" element={<NotFound />} />
-          {/* <PrivateRoute path="/dashboard"  element={<Dashboard />} /> */}
-          {/* <Route path="*" render={() => <Navigate to="/login" />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
