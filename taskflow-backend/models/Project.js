cfea466089b15fa4a0499de8b7f4b02057f4bfe3
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2025-07-04 12:48:56 +0100

    feat(user): add role for when user is created

--- taskflow-backend/models/Project.js
+++ taskflow-backend/models/Project.js
@@ -1,11 +1,39 @@
 const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  members: [{ userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, role: String }],
-  createdAt: { type: Date, default: Date.now }
+  name: { 
+    type: String, 
+    required: true, //[true, 'Project name is required']
+    trim: true
+  },
+  description: { 
+    type: String,
+    trim: true 
+  },
+  createdBy: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  members: [
+    { 
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
+      role: {type: String, default: 'Member' }
+    }
+  ],
+  status: {
+    type: String,
+    enum: ['Not Started', 'In Progress', 'Completed'],
+    default: 'Not Started'
+  },
+  startDate: {
+    type: Date,
+  },
+  endDate: {
+    type: Date,
+  }
+}, { 
+  timestamps: true
 });
 
 module.exports = mongoose.model('Project', projectSchema);
--- taskflow-backend/models/User.js
+++ taskflow-backend/models/User.js
@@ -5,19 +5,17 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['admin', 'member'], default: 'member' }
+  role: { type: String, enum: ['Admin', 'Member'], default: 'Member' }
 });
 
-// Hash password before save
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Compare password
 userSchema.methods.comparePassword = function (password) {
-    return bcrypt.compare(password, this.password);
+  return bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
