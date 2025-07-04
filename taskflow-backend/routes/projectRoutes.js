9002a1f420f4f03f729e2fd9e3ad1de26c177329
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2025-07-04 12:50:58 +0100

    add more routes

--- taskflow-backend/routes/projectRoutes.js
+++ taskflow-backend/routes/projectRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { protect } = require('../middlewares/authMiddleware');
-const { getProjects, createProject } = require('../controllers/projectController');
 const router = express.Router();
+const { getProjects, getProjectById, createProject, inviteMember } = require('../controllers/projectController');
+const { protect } = require('../middlewares/authMiddleware');
 
 router.use(protect);
-router.get('/', getProjects);
-router.post('/', createProject);
+router.get('/get', getProjects);
+router.post('/create', createProject);
+router.get('/:id', getProjectById);
+router.post('/:id/invite', inviteMember);
 
 module.exports = router;
