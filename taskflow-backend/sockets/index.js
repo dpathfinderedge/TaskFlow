5cf423a7902f7abf2ab32da8b532367d29a1e57b
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2025-07-11 09:58:29 +0100

    feat(socket): set up Socket.IO server for real-time collaboration

--- taskflow-backend/sockets/index.js
+++ taskflow-backend/sockets/index.js
@@ -1,31 +1,47 @@
-let io;
+// let io;
 
-module.exports = {
-  init: (server) => {
-    io = require('socket.io')(server, {
-      cors: {
-        origin: 'http://localhost:5173',
-        credentials: true
-      }
+// module.exports = {
+//   init: (server) => {
+//     io = require('socket.io')(server, {
+//       cors: {
+//         origin: 'http://localhost:5173',
+//         credentials: true
+//       }
+//     });
+
+//     io.on('connection', (socket) => {
+//       console.log('Client connected:', socket.id);
+
+//       // join a room for project list updates
+//       socket.on('joinProjects', () => {
+//         socket.join('projectsRoom');
+//       });
+
+//       socket.on('disconnect', () => {
+//         console.log('Client disconnected:', socket.id);
+//       });
+//     });
+
+//     return io;
+//   },
+//   getIO: () => {
+//     if (!io) throw new Error('Socket.io not initialized!');
+//     return io;
+//   }
+// };
+
+
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
+
+    socket.on('join-project', (projectId) => {
+      socket.join(`project_${projectId}`);
+      console.log(`Socket ${socket.id} joined project_${projectId}`);
     });
 
-    io.on('connection', (socket) => {
-      console.log('Client connected:', socket.id);
-
-      // join a room for project list updates
-      socket.on('joinProjects', () => {
-        socket.join('projectsRoom');
-      });
-
-      socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-      });
+    socket.on('disconnect', () => {
+      console.log('Client disconnected:', socket.id);
     });
-
-    return io;
-  },
-  getIO: () => {
-    if (!io) throw new Error('Socket.io not initialized!');
-    return io;
-  }
+  });
 };
