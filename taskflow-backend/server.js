d9bd8bd5fc711277143dad036245f1fb21a10c48
Rasheed Shodiya <shodiyarasheed100@gmail.com>
2025-07-12 09:19:50 +0100

    feat(socket): set up Socket.IO server for real-time collaboration

--- taskflow-backend/server.js
+++ taskflow-backend/server.js
@@ -1,24 +1,44 @@
-require('dotenv').config();
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const registerSocketEvents = require('./sockets');
 
 const app = express();
+const server = http.createServer(app);
+
+// Setup up Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  },
+});
+registerSocketEvents(io);
+
+app.set('io', io);
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`Connected to backend`))
+    server.listen(PORT, () => console.log(`Connected to backend`))
   })
   .catch((err) => console.log(err));
-
 
